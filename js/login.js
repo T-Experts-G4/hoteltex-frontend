0e6cbe902ba5150ec4d1f44fb779d71e56d1fe58
Rjj18 <roger.psn12@gmail.com>
2023-01-14 23:46:50 -0300

    implantacao localstorage

--- js/login.js
+++ js/login.js
@@ -2,21 +2,66 @@ const user = document.getElementById("user");
 const password = document.getElementById("password");
 const btnLogin = document.getElementById("btnLogin");
 const btnFuncionario = document.getElementById("btnLoginFun");
-
 const funcionario = {user:"admin", password:"admin"};
 const cliente = {user:"client", password:"client"};
 
-btnLogin.addEventListener("click", (e) => {
-    e.preventDefault();
 
+
+// Teste Local storage
+
+function testeLocal () {
     if (user.value === cliente.user && password.value === cliente.password) {
+        localStorage.setItem("userB","cliente");
         window.location.href = "./admin/reservas_admin.html";
-        console.log("Logado");
     }else if (user.value === funcionario.user && password.value === funcionario.password){
+        localStorage.setItem("user","funcionario");
         window.location.href = "./admin/home_admin.html";
-        console.log("Logado");
     }else {
-        console.log("Usuario e senha invalida");
+        alert("Usuario e/ou senha invalida");
     }
-})
+}
 
+// Login Home e Reservas
+
+const nomeUsuarioADM = document.getElementById("nome-usuario");
+const nomeUsuarioCliente = document.getElementById("nome-usuario-reserva")
+const nomeLogadoADM = localStorage.getItem("user");
+const nomeLogadoCliente = localStorage.getItem("userB");
+
+
+nomeUsuarioADM.innerHTML = nomeLogadoADM;
+nomeUsuarioCliente.innerHTML = nomeLogadoCliente;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// btnLogin.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     if (user.value === cliente.user && password.value === cliente.password) {
+//         window.location.href = "./admin/reservas_admin.html";
+//         console.log("Logado");
+//     }else if (user.value === funcionario.user && password.value === funcionario.password){
+//         localStorage.setItem("user","funcionario");
+//         window.location.href = "./admin/home_admin.html";
+//         console.log("Logado");
+//     }else {
+//         alert("Usuario e/ou senha invalida");
+//     }
+// })
