bf4d9b9847287467d3dc3a7f26d70fc6b9906252
Armando Freitas <cnxarmando@gmail.com>
2023-01-17 10:41:36 -0300

    Criada as funçoes para inserir dados no no campo e para limpar localStorage

--- js/user.js
+++ js/user.js
@@ -19,6 +19,28 @@ document.getElementById('criancas').addEventListener('change',function(){localSt
 console.log(quarto.value)
 
 // alimentando o resumo da reserva
-resumoCheckin.innerText = localStorage.checkin.split('-').reverse().join('/')
-resumoCheckout.innerText = localStorage.checkout.split('-').reverse().join('/')
-resumoPessoas.innerText = parseInt(localStorage.adultos) + parseInt(localStorage.criancas)
+let resumoCheckin = document.querySelector('#resumoCheckin');
+let resumoCheckout = document.querySelector('#resumoCheckout');
+let resumoPessoas = document.querySelector('#resumoPessoas');
+
+function insereDadosReserva() {
+  resumoCheckin.innerText = localStorage.checkin.split('-').reverse().join('/')
+  resumoCheckout.innerText = localStorage.checkout.split('-').reverse().join('/')
+  resumoPessoas.innerText = parseInt(localStorage.adultos) + parseInt(localStorage.criancas)
+}
+
+// Monitorando o formulário
+const formulario = document.querySelector('#formulario')
+formulario.addEventListener('change', function() {
+  insereDadosReserva()
+})
+
+//limpando o local storage
+window.addEventListener('load', function() {
+localStorage.removeItem('checkin')
+  localStorage.removeItem('checkout')
+  localStorage.removeItem('adultos')
+  localStorage.removeItem('criancas')
+})
+
+
