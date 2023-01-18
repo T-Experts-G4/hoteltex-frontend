64d60201f700897853cc28eb0cd338edf77815be
Volnei Alves <volneialves.f@gmail.com>
2023-01-18 17:24:59 -0300

    Update validateAuth.js

--- js/validateAuth.js
+++ js/validateAuth.js
@@ -1,7 +1,7 @@
 const token = localStorage.getItem('token')
 
 if(!token){
-    window.location.href = './pages/login.html'
+    window.location.href = './../pages/login.html'
 }
 
 function logout(){
