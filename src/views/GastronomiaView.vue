69d8ad9f6c7459598a937e781df56df366c33da5
Fidel <fidel.sc@hotmail.com>
2023-01-31 11:14:24 -0300

    adição HTML pagina gastronomia

--- src/views/GastronomiaView.vue
+++ src/views/GastronomiaView.vue
@@ -1,9 +1,38 @@
 <template>
 <HeaderComponent />
 <NavbarComponent />
-  <div class="gastronomia">
-    <h1>gastronomia</h1>
-  </div>
+ <main>
+      <section>
+        <h2 class="gastronomia_titulo">Gastronomia</h2>
+        <div class="gastronomia_img">
+            <div>
+                <img src="../assets/imagens/gastronomia/gastronomia.jpg" alt="">
+            </div>
+            <div>
+                <img src="../assets/imagens/gastronomia/drink.jpg" alt="">
+            </div>
+            <div>
+                <img src="../assets/imagens/gastronomia/gastronomia04.jpg" alt="">
+            </div>
+        </div>
+        <div class="gastronomia_container01">
+            <div>
+                <img src="../assets/imagens/gastronomia/restaurante02.jpg" alt="">
+            </div>
+            <div class="gastronomia_box01">
+                <p>ipsum dolor, sit amet consectetur adipisicing elit. Itaque voluptatem accusamus non facilis nemo necessitatibus at eveniet sapiente dolorem provident? Libero, odit. Officia rem laborum, deserunt, laboriosam quidem temporibus dolores quaerat quas maxime tenetur aliquid cupiditate, voluptatum itaque deleniti similique.</p>
+            </div>
+        </div>
+        <div class="gastronomia_container01">
+            <div>
+               <img src="../assets/imagens/gastronomia/bar.jpg" alt="">
+            </div>
+            <div class="gastronomia_box01">
+                ipsum dolor sit amet consectetur adipisicing elit. Quam laboriosam quisquam iusto quia debitis cumque necessitatibus nisi accusamus vero, assumenda quibusdam recusandae sed accusantium ad dolor quo quas qui sunt aut aliquid est error vel sint! Voluptatum voluptatibus fugit quae?
+            </div>
+        </div>
+      </section>
+    </main>
 <FooterComponent />
 </template>
 
