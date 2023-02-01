c0e0fadc749fda7aba67fcdd8ad322037515fad6
Fidel <fidel.sc@hotmail.com>
2023-02-01 10:18:55 -0300

    adição JS do botão login e reforma da rota

---
+++ src/components/BotaoLoginJS.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  name: 'BotaoLoginJS',
+  mounted () {
+    const user = document.getElementById('user')
+    const password = document.getElementById('password')
+    const btnConfirma = document.getElementById('btnConfirma')
+    const erroAutentic = document.getElementById('erroAutentic')
+
+    const funcionario = { user: 'admin', password: 'admin' }
+    const cliente = { user: 'client', password: 'client' }
+
+    btnConfirma.addEventListener('click', (e) => {
+      e.preventDefault()
+
+      if (user.value === cliente.user && password.value === cliente.password) {
+        // window.location.href = "../views/HomeView.vue";
+        localStorage.setItem('user', cliente.user)
+        console.log('Logado')
+      } else if (user.value === funcionario.user && password.value === funcionario.password) {
+        localStorage.setItem('user', funcionario.user)
+        window.location.href = './admin/home_admin.html'
+
+      } else {
+        console.log('Usuario e senha invalida')
+        erroAutentic.classList.remove('d-none')
+        }
+    })
+}
+}
+</script>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,28 +1,30 @@
 <template>
-<HeaderComponent />
-<NavbarComponent />
-  <main>
-        <div class="estiloLogin imagemPraia">
-            <p class="estiloFonte">Login</p>
-            <form action="" method="post">
-                <label class="estilo__fonte--input" for="nome_usuario">Email</label>
-                <input class="estiloInput" type="text" name="emailUsuario" id="user">
-                <label class="estilo__fonte--input" for="nome_usuario">Senha</label>
-                <input class="estiloInput " type="password" name="senhaUsuario" id="password">
-                <div class="centralizaBotao">
-                    <button type="button" class="estiloBotao" id="btnConfirma">Confirmar</button>
-                </div>
-            </form>
-            <div class="centralizaSenha">
-                <div class="estiloSenha">
-                    <p><a href="./emConstrucao.html">Esqueceu sua senha?</a></p>
+    <HeaderJS />
+    <HeaderComponent />
+    <NavbarComponent />
+    <main>
+            <div class="estiloLogin imagemPraia">
+                <p class="estiloFonte">Login</p>
+                <form action="" method="post">
+                    <label class="estilo__fonte--input" for="nome_usuario">Email</label>
+                    <input class="estiloInput" type="text" name="emailUsuario" id="user">
+                    <label class="estilo__fonte--input" for="nome_usuario">Senha</label>
+                    <input class="estiloInput " type="password" name="senhaUsuario" id="password">
+                    <div class="centralizaBotao">
+                        <!-- <button type="button" class="estiloBotao" id="btnConfirma">Confirmar</button> -->
+                        <router-link to="/" id="btnConfirma" class="estiloBotao">INICIO</router-link>
+                    </div>
+                </form>
+                <div class="centralizaSenha">
+                    <div class="estiloSenha">
+                        <p><a href="./emConstrucao.html">Esqueceu sua senha?</a></p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div style="width: 200px;" class="alert alert-danger mx-auto d-none" role="alert" id="erroAutentic">
-           <span class="mx-auto text-justify">Usuario e senha invalida</span>
-        </div>
-    </main>
+            <div style="width: 200px;" class="alert alert-danger mx-auto d-none" role="alert" id="erroAutentic">
+            <span class="mx-auto text-justify">Usuario e senha invalida</span>
+            </div>
+        </main>
 <FooterComponent />
 </template>
 
@@ -31,9 +33,10 @@
 import NavbarComponent from '@/components/NavbarComponent.vue'
 import HeaderComponent from '@/components/HeaderComponent.vue'
 import FooterComponent from '@/components/FooterComponent.vue'
+import BotaoLoginJS from '@/components/BotaoLoginJS.vue'
 
 export default {
   name: 'LoginView',
-  components: { NavbarComponent, HeaderComponent, FooterComponent }
+  components: { NavbarComponent, HeaderComponent, FooterComponent, BotaoLoginJS }
 }
 </script>
